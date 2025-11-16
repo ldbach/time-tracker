c79e90d5f33f2790158c0285b70317dd9cfbd4c6
ldbach <ducbach1999@gmail.com>
2025-11-16 22:53:03 +0100

    highlight the link

--- time-tracker-frontend/src/App.js
+++ time-tracker-frontend/src/App.js
@@ -1,25 +1,48 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, NavLink, Navigate } from "react-router-dom";
 import TimeTracker from "./pages/TimeTracker";
 import WorkflowDesign from "./pages/WorkflowDesign";
 
 function App() {
+  const linkStyle = {
+    marginRight: "10px",
+    textDecoration: "none",
+    color: "blue",
+  };
+
   return (
     <Router>
       <div style={{ textAlign: "center", marginTop: "50px" }}>
         {/* Navigation */}
         <nav style={{ marginBottom: "20px" }}>
-          <Link to="/" style={{ marginRight: "10px" }}>Time Tracker</Link>
-          <Link to="/workflow-design">Workflow Design</Link>
+          <NavLink
+            to="/"
+            end
+            style={({ isActive }) => ({
+              ...linkStyle,
+              fontWeight: isActive ? "bold" : "normal",
+              color: isActive ? "darkblue" : "blue",
+            })}
+          >
+            Time Tracker
+          </NavLink>
+          <NavLink
+            to="/workflow-design"
+            style={({ isActive }) => ({
+              ...linkStyle,
+              fontWeight: isActive ? "bold" : "normal",
+              color: isActive ? "darkblue" : "blue",
+            })}
+          >
+            Workflow Design
+          </NavLink>
         </nav>
 
         <Routes>
           {/* Default route */}
           <Route path="/" element={<TimeTracker />} />
-
           {/* Workflow route */}
           <Route path="/workflow-design" element={<WorkflowDesign />} />
-
           {/* Redirect unknown routes to default */}
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
